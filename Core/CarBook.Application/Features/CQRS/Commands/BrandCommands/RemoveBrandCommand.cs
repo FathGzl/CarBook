d5a994103b99bdb613ac2b825e7eb01b343a4512
FathGzl <fatihguzel3455@gmail.com>
2024-02-14 14:08:05 +0300

    Contact CQRS ve Api Test işlemleri

--- Core/CarBook.Application/Features/CQRS/Commands/BrandCommands/RemoveBrandCommand.cs
+++ Core/CarBook.Application/Features/CQRS/Commands/BrandCommands/RemoveBrandCommand.cs
@@ -2,11 +2,11 @@
 {
     public class RemoveBrandCommand
     {
+        public int Id { get; set; }
         public RemoveBrandCommand(int id)
         {
             Id = id;
         }
 
-        public int Id { get; set; }
     }
 }
